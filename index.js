ed31d72825e1ff6851ca083f14e2254789d001cb
Miquel Castro <castro-miquel@hotmail.com>
2021-04-01 19:50:19 +0200

    variable entorno AUT y URL API TMB

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fetch = require("node-fetch");
 
 const app = express();
 
-const server = app.listen(5000, () => {
+app.listen(5000, () => {
   console.log(chalk.yellow("Servidor escuchando en el puerto 5000."));
 });
 
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/metro/lineas", (req, res, next) => {
-  fetch("https://api.tmb.cat/v1/transit/linies/metro?app_id=a372a6d9&app_key=de3506372e19c90a75a39c1fa2dc9fb7")
+  fetch(`${process.env.APP_TMB_API_URL_METRO}?${process.env.APP_TMB_API_AUT}`)
     .then(res => res.json())
     .then(lineas => {
       res.json(lineas.features.map(linea => ({
@@ -34,14 +34,14 @@ app.get("/metro/lineas", (req, res, next) => {
 
 app.get("/metro/linea/:nombre", (req, res, next) => {
   const { nombre } = req.params;
-  fetch(`https://api.tmb.cat/v1/transit/linies/metro?filter=NOM_LINIA='${nombre}'&app_id=a372a6d9&app_key=de3506372e19c90a75a39c1fa2dc9fb7`)
+  fetch(`${process.env.APP_TMB_API_URL_METRO}?filter=NOM_LINIA='${nombre}'&${process.env.APP_TMB_API_AUT}`)
     .then(res => res.json())
     .then(linea => {
       if (linea.numberMatched === 0) {
         res.status(404).json({ error: true, mensaje: "Recurso no encontrado" });
       } else {
         const { CODI_LINIA, NOM_LINIA, DESC_LINIA } = linea.features[0].properties;
-        fetch(`https://api.tmb.cat/v1/transit/linies/metro/${CODI_LINIA}/estacions?app_id=a372a6d9&app_key=de3506372e19c90a75a39c1fa2dc9fb7`)
+        fetch(`${process.env.APP_TMB_API_URL_METRO}/${CODI_LINIA}/estacions?${process.env.APP_TMB_API_AUT}`)
           .then(res => res.json())
           .then(paradasResp => {
             res.json(
